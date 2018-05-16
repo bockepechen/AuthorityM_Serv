8e9f1340a29fdac4aef0ee32e2e6bbd127ecb76f
bockepechen@163.com <123.com>
2018-05-16 17:05:45 +0800

    ZDAuthorityM_Serv_UPD_20180516_003 添加新的返回码

--- src/com/am/utils/ReturnCodeUtil.java
+++ src/com/am/utils/ReturnCodeUtil.java
@@ -6,11 +6,12 @@ import com.jfinal.log.Log;
  * Created by ZHAO on 2018/5/14.
  */
 public class  ReturnCodeUtil {
-	public  static String returnCode = "0000";//成功
-	public  static String returnCode1 = "0001";//登录失败
-	public  static String returnCode2 = "9999";//请求异常
-	public  static String returnCode3 = "9998";//数据格式不正确
-	public  static String returnCode4 = "0002";//当前系统无该用户
+	public static String returnCode = "0000";//成功
+	public static String returnCode1 = "0001";//登录失败
+	public static String returnCode2 = "9999";//请求异常
+	public static String returnCode3 = "9998";//数据格式不正确
+	public static String returnCode4 = "0002";//当前系统无该用户
+	public static String returnCode5 = "0003"; // 机构名称已经存在
 	static Log log = Log.getLog(ReturnCodeUtil.class);
 
 
